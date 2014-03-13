f5b5ab1263a5e637971d0b540fe3e215c8de4c9d
kaifius <kai.hofius@gmail.com>
2014-03-13 15:53:09 -0700

    first commit - app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WdiFinalProject::Application.config.secret_key_base = 'd5978c6802c9678635a24a8df74133e6293f5e7bb6c57a3c584e66c5c24e4d826978566f410dd240ba6e02ed5bc3deca02430f227b419c500ce7ab77b395f8d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WdiFinalProject::Application.config.session_store :cookie_store, key: '_wdi_final_project_session'
